a46107fc353cbbc644b7fc79f741a3624992e863
andrewgray0702 <andrew.gray@midlandu.edu>
2019-04-19 15:54:40 -0500

    minor fixes

--- firstFakeApp/src/app/pipes/searchby.pipe.ts
+++ firstFakeApp/src/app/pipes/searchby.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchbyPipe implements PipeTransform {
 
   transform(value: Object[], type: string): any {
-
+    if(type == "fish"){
+      return value.filter(value => value['type'].includes("fish"));
+    } else if(type == "coral"){
+      return value.filter(value => value['type'].inclues("coral"))
+    }
   }
 
 }
